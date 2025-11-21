ee78cf806b1f3c79940f12e8ca5c2abbaf6b6e9e
lauchnor lamb <llamb@eagles.bridgewater.edu>
2025-11-21 12:48:45 -0500

    updating repo

--- words.py
+++ words.py
@@ -1,1 +1,12 @@
-NUM_LETTERS
+def num_letters(filename):
+    f = open(filename, "r")
+    count = 0
+    for line in f:
+        for char in line:
+            if ('A' <= char <= 'Z') or ('a' <= char <= 'z'):
+                count += 1
+    f.close()
+    return count
+
+assert 
+print("Checking assertions.")
