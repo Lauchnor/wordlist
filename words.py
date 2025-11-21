0d8c4c85c5c53588a8b536209e5cfa7f84c739e5
lauchnor lamb <llamb@eagles.bridgewater.edu>
2025-11-21 13:27:16 -0500

    updating repo

--- words.py
+++ words.py
@@ -1,12 +1,28 @@
-def num_letters(filename):
-    f = open(filename, "r")
-    count = 0
-    for line in f:
-        for char in line:
-            if ('A' <= char <= 'Z') or ('a' <= char <= 'z'):
-                count += 1
-    f.close()
-    return count
+def num_letters(n_row,n_col):
+    return n_col*2 + n_row*2 - 4
 
-assert 
-print("Checking assertions.")
+##---Part 3---##
+import numpy as np
+data = np.loadtxt("loop4.csv", delimiter=",", dtype=str, encoding='utf8')
+
+words = []
+rows, columns = data.shape
+
+for c in range(columns):
+    words.append(data[0, c])
+
+for r in range(1, rows - 1):
+    words.append(data[r, columns - 1])
+
+for c in range(columns - 1, -1, -1):
+    words.append(data[rows - 1, c])
+
+for r in range(rows - 2, 0, -1):
+    words.append(data[r, 0])
+
+print(words)
+
+assert len(words) == num_letters(rows, columns)
+
+
+##---Part 4---##
